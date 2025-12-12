fe85b6c188fe6c55d80e8d77d72ba0488e2d4ed5
muthu010110 <muthukarunanithi2003@gmail.com>
2025-12-12 15:25:12 +0530

    Update Problem-4.java

--- Problem-4.java
+++ Problem-4.java
@@ -1,1 +1,21 @@
-
+public class Problem4 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 8, 9, 12, 46, 76, 82, 15, 20, 30};
+        int[] counts = new int[10]; 
+        for (int num : arr) {
+            for (int i = 1; i <= 9; i++) {
+                if (num % i == 0) {
+                    counts[i]++;
+                }
+            }
+        }
+        System.out.print("{");
+        for (int i = 1; i <= 9; i++) {
+            System.out.print(i + ": " + counts[i]);
+            if (i < 9) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("}");
+    }
+}
