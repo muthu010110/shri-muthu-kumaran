2745c2c1b005fe2a6722f7c7ad3f04574645d285
muthu010110 <muthukarunanithi2003@gmail.com>
2025-12-12 15:23:30 +0530

    Update Problem-3.java

--- Problem-3.java
+++ Problem-3.java
@@ -1,1 +1,23 @@
 
+import java.util.Scanner;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = sc.nextInt();
+        int limit;
+        if (a % 2 == 0) {
+            limit = a - 1;
+        } else {
+            limit = a;     
+        }
+        for (int i = 1; i <= limit; i += 2) {
+            System.out.print(i);
+            if (i + 2 <= limit) {
+                System.out.print(", ");
+            }
+        }
+        sc.close();
+    }
+}
