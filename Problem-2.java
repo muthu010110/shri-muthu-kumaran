b87f49c1237eece84010deb1cbaa6a51256136b0
muthu010110 <muthukarunanithi2003@gmail.com>
2025-12-12 15:22:35 +0530

    Update Problem-2.java

--- Problem-2.java
+++ Problem-2.java
@@ -1,1 +1,19 @@
 
+import java.util.Scanner;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = sc.nextInt();
+        int num = 1;
+        for (int i = 1; i <= a; i++) {
+            System.out.print(num);
+            if (i < a) {
+                System.out.print(", ");
+            }
+            num += 2;  
+        }
+        sc.close();
+    }
+}
